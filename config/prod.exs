2020d0d369ce0a4cdc05bea0d5a6215e5ce83e8d
Tim Mecklem <tim@teamgaslight.com>
2016-12-03 12:58:44 -0500

    Add prod database config

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,12 @@ config :ex_diagcare, ExDiagcare.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
+config :ex_diagcare, ExDiagcare.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # Do not print debug messages in production
 config :logger, level: :info
 
